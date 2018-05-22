e82774e674fdf47a77d868dfa423fdc8899f8a7c
Jesse Tilro <jesse@jessetilro.nl>
2018-05-22 22:49:57 +0200

    Update compile scripts to include JAMA library.

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-javac -cp jars/javaview.jar:jars/jvLite.jar:jars/jvx.jar:. workshop/*.java
-javac -cp jars/javaview.jar:jars/jvLite.jar:jars/jvx.jar:. menu/*.java
+javac -cp jars/javaview.jar:jars/jvLite.jar:jars/jvx.jar:jars/Jama-1.0.3.jar:. workshop/*.java
+javac -cp jars/javaview.jar:jars/jvLite.jar:jars/jvx.jar:jars/Jama-1.0.3.jar:. menu/*.java
